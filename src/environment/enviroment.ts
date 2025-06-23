6022ab383a9fcaf00d726d488b2d10ae5aa89855
Guilherme <guilhermepontifice.gp@gmail.com>
2025-06-23 19:21:21 -0300

    fix: Token Exposto

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -3,11 +3,12 @@
 //     API_URL: 'https://freeapi.miniprojectideas.com/api/ClientStrive/'
 // };
 
+import { secret } from "./environment.secret";
+
 export const environment = {
     production: false,
     API_URL: 'https://api.themoviedb.org/3',
-    API_TOKEN: process.env['API_THEMOVIEDB_TOKEN']
-    // API_TOKEN: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGE2MDg2ODAzYTc4NzE5M2M5NTk1NDE1YzA0MTc4YiIsIm5iZiI6MTY5MjM4NzA1MS4yMjIwMDAxLCJzdWIiOiI2NGRmYzZlYmI3N2Q0YjExM2UwNmY0YTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.k2R1VeSC42fXqxkP2gOPMEoGG8RTfP3Fi9qzK8_3uw4"
+    API_TOKEN: secret.API_TOKEN,
 }
 const options = {
     method: 'GET',
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://api.themoviedb.org/3',
+  API_TOKEN: ''
+};
